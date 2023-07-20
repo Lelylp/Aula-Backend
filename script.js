63c16855d3c186210414d1027dba0e7387850a5d
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-07-20 12:20:03 +0000

    aula 20/07

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-console.log("Olá mundo !");
-alert("olá");
+var minhaVar;
+minhaVar = 10;
+
+var soma;
+soma = 15+8;
+
+soma += 1;
+soma *= 3;
+
+//soma é 72
+
+var fazSol = true;
+
+var comida = ['arroz', 'feijão', 'ovo'];
+
+console.log(comida[1]);
+
+if(soma === minhaVar){
+    console.log("É igual");
+}else{
+    console.log("Não é");
+}
+
+if(minhaVar<=soma){
+    console.log("minhaVar é menor ou igual à soma");
+}else{
+    console.log("Não são iguais");
+}
+
+function divisao(a, b){
+    return a/b;
+}
+
+console.log(divisao(10,2));
