2445f0cb9bed415721218d0f2e272ced1b096aa5
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-07-25 11:57:48 +0000

    nem lembro mais qual é o commit kkk

---
+++ script4.js
@@ -0,0 +1,9 @@
+let idade = 15;
+
+if (idade <16) {
+    console.log("Não pode dirigir");
+} else if(idade <=17){
+    console.log("Pode dirigir mas com alguma coisa");
+}else{
+    console.log("Pode dirigir de boa");
+}
