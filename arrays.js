32fbab517092c87fc82738842328a9cd4cfd1cac
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-08-01 12:57:36 +0000

    add arrays

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const meuArray = [10, 20, 30, 40, 50];
+let segundoElemento = meuArray[1];
+console.log(segundoElemento);
+
+const frutas = ['maça', 'banana', 'laranja', 'uva', 'manga'];
+frutas.push('pera');
+frutas.splice(0,1)
+console.log(frutas);
+
+const numeros = [10, 20, 30, 40, 50];
+console.log(numeros);
+console.log(`Existe número 25? ${numeros.includes(25)}`);
