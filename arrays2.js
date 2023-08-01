0fe6455e239a00610e4c44337d770b13ccaa4c17
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-08-01 14:00:51 +0000

    add arrays2

---
+++ arrays2.js
@@ -0,0 +1,31 @@
+//quest 1
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [1, 2, 3, 4, 5];
+let arrayJuntos = array1.concat(array2);
+console.log("Concat de 2 arrays");
+console.log(arrayJuntos);
+
+//quest 2
+const numeros1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Números páres");
+console.log(numeros1.filter(x => x % 2 === 0));
+
+//quest 3
+const numeros2 = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+console.log("Números ordenados");
+console.log(numeros2.sort());
+
+//quest 4
+const frutas = ['maçã', 'banana', 'laranja', 'uva', 'manga'];
+console.log(`O index onde está laranja é ${frutas.indexOf('laranja')}`);
+let num = 1
+//quest 5
+
+function ehArray (array) {
+    if (typeof array == 'object') {
+        return true
+    }else{
+        return false
+    }
+}
+console.log(`variavel frutas é um array? ${ehArray(frutas)}`);
