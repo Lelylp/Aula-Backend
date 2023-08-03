77997e0cf55ced694ea7790516bf0f55946ca193
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-08-03 14:02:35 +0000

    add do-while

---
+++ js/do-while.js
@@ -0,0 +1,86 @@
+//quest 1
+let num = 1;
+do {
+    console.log(num);
+    num++; 
+} while (num <= 10);
+
+//quest 2
+num = 1;
+let soma = 0;
+do {
+    soma += num;
+    num++;
+} while (num <= 100);
+console.log(soma);
+
+//quest 3
+num = 1;
+let contPares = 0;
+do {
+    if (num%2 == 0) {
+        contPares += 1;
+    }
+    num++;
+} while (num != 50);
+console.log(contPares);
+
+//quest 4
+num = 1;
+do {
+    num *= 2;
+} while (num <= 1000);
+console.log(num);
+
+//quest 5
+num = require('readline-sync').questionInt("Digite um número: ");
+if (num%2 !== 0 && num%3 !== 0) {
+    console.log("Seu número é um número primo");
+} else {
+    console.log("Seu número não é um número primo");
+}
+
+//quest 6
+let senha = 1234;
+do {
+    var pass = require('readline-sync').questionInt("Digite sua senha: ");
+    if (senha !== pass) {
+        console.log("Senha incorreta, tente novamente");
+    }
+} while (senha !== pass);
+console.log("Senha correta");
+
+//quest 7
+num = 3;
+do {
+    if (num %3 === 0) {
+        console.log(num);
+    }
+    num += 3;
+} while (num <= 30);
+
+//quest 8
+let numeros = [1, 2, 3, 4, 5, 6];
+let media = 0, i = 0;
+do {
+    media += numeros[i];
+    i++
+} while (i < numeros.length);
+media /= numeros.length
+console.log(media);
+
+//quest 9
+num = require('readline-sync').questionInt("Digite um número para saber sua fatorial: ")
+let fatorial = 1;
+do {
+    fatorial *= num;
+    num--;
+} while (num !=0);
+console.log(fatorial);
+
+//quest 10
+num = 10;
+do {
+    console.log(num);
+    num--;
+} while (num >0);
