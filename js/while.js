c6e500bb454abc1abe6ca84205c095be081f0b47
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-08-03 15:31:29 +0000

    add

--- js/while.js
+++ js/while.js
@@ -34,7 +34,7 @@ console.log(num);
 
 //quest 5
 num = require('readline-sync').questionInt("Digite um número: ");
-if ((num == 2 || num == 3) || (num%2 !== 0 && num%3 !== 0 && num%5 !==0)) {
+if ((num == 2 || num == 3 || num == 5) || (num%2 !== 0 && num%3 !== 0 && num%5 !==0)) {
     console.log("Seu número é um número primo");
 }else {
     console.log("Seu número não é um número primo");
