7d21dca712a8d5972e4e6e9419bc9dbecd04413a
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-08-17 14:09:35 +0000

    add mais coisas trycatch

--- js/try.js
+++ js/try.js
@@ -1,4 +1,4 @@
-try {
+/* try {
     let resultado = 10/0;
     if (resultado === Infinity) {
         throw 'Sem zero amigao';
@@ -8,4 +8,41 @@ try {
     // console.error("Erro: ", error.message);
     console.error("Erro: ", error);
 }
-//console.log("O resultado é:", resultado);
+console.log("O resultado é:", resultado); */
+
+//quest 1
+try {
+    let num = require('readline-sync').question("Digite um número: ");
+    if (isNaN(num)) {
+        throw 'Somente números'
+    }
+    let resultado = Number(num)/0;
+    if (resultado === Infinity) {
+        throw 'Divisão por zero'
+    }
+} catch (e) {
+    console.error("Erro:", e)
+}
+//quest 2
+try {
+    let texto = require('readline-sync').question("Digite um texto: ");
+    texto = parseInt(texto);
+    if (isNaN(texto)) {
+        throw 'Conversão inválida'
+    }
+} catch (e) {
+    console.error("Erro:", e);
+}
+//quest 3
+try {
+    let pessoa = {
+        nome: require('readline-sync').question("Digite seu nome: "),
+        idade: require("readline-sync").questionInt("Digite sua idade: "),
+    }
+    let variavel = console.log(pessoa.carteira);
+    if (variavel == undefined) {
+        throw 'Propriedade não encontrada'
+    }
+} catch (error) {
+    console.error("Erro:", error);
+}
