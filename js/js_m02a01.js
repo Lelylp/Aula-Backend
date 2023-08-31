eeef9a6b67b7677227a74ee872921b5c60800f17
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-08-31 13:42:57 +0000

    terminado

--- js/js_m02a01.js
+++ js/js_m02a01.js
@@ -12,7 +12,7 @@ console.log(media);
 
 //quest 2
 let ano = require("readline-sync").questionInt("Digite um ano: ");
-if (ano % 4 === 0 && ano % 100 !== 0) {
+if (ano % 4 === 0 && ano % 100 !== 0 || ano % 400 === 0) {
     console.log("Esse ano é bissexto");    
 } else {
     console.log("Esse ano não é bissexto");
@@ -88,6 +88,7 @@ while (num<n) {
 let salarioLiquido = (numeroTrabalhados*30)*0.92;
 console.log(`Dias: ${numeroTrabalhados} = Salario: ${salarioLiquido} R$`);
  */
+
 //quest 7
 /* let ano = 2023, mes = 8, dia = 29, dia2 = 30;
 let hora = require('readline-sync').questionInt("Digite a hora: ")
@@ -98,10 +99,28 @@ let horaTermino = require('readline-sync').questionInt("Digite a hora do Termino
 let minutosTermino = require('readline-sync').questionInt("Digite os minutos do Termino: ")
 let segundosTermino = require('readline-sync').questionInt("Digite os segundos do Termino: ")
 let horarioTermino = new Date(ano, mes, dia2, horaTermino, minutosTermino, segundosTermino);
-let diferença = (-1)*(horarioInicio-horarioTermino);
-let dias = diferença/1000/60/60/24 */
+let diff = Math.abs(horarioInicio.getTime() - horarioTermino.getTime());
+let diffSeg = Math.ceil(diff/1000%60);
+let diffMin = Math.floor(diff/1000/60%60);
+let diffHora = Math.floor(diff/1000/60/60%24);
+console.log(`${diffHora}:${diffMin}:${diffSeg}`); */
 
-let temperatura = require('readline-sync').questionFloat("Digite a temperatura: ");
+
+//quest 8
+/* let horaNormal = require('readline-sync').questionInt("Digite a quantidade de horas trabalhadas por dia: ");
+let horaExtra = require('readline-sync').questionInt("Digite a quantidade de horas extras trabalhadas: ");
+let salario = (horaNormal*10.00*24*12) + (horaExtra * 15); */
+
+//quest 9
+/* let numeros = Array(3);
+for (let i = 0; i < numeros.length; i++) {
+    numeros[i] = require('readline-sync').questionInt("Digite um número: ")
+}
+numeros.sort((a,b)=>a-b);
+console.log(numeros); */
+
+//quest 10
+/* let temperatura = require('readline-sync').questionFloat("Digite a temperatura: ");
 let opcao = require('readline-sync').questionInt("1.de Celsius para Fahrenheit.\n2.de Celsius para Kelvin.\n3.de Fahrenheit para Celsius.\n4.de Fahrenheit para Kelvin.\n5.de Kelvin para Celsius.\n6.de Kelvin para Fahrenheit:\n");
 switch (opcao) {
 
@@ -130,4 +149,76 @@ switch (opcao) {
     default:
         console.log("Número errado");
         break;
+} */
+
+//quest 11
+/* function mesAno(mes,ano) {
+        switch (mes) {
+            case 1:
+                return 31;
+            case 2:
+                if (ano % 4 === 0 && ano % 100 !== 0 || ano % 400 ===0) {
+                    return 29;
+                }
+                return 28;
+            case 3:
+                return 31;
+            case 4:
+                return 30;
+            case 5:
+                return 31;
+            case 6:
+                return 30;
+            case 7:
+                return 31;
+            case 8:
+                return 31;
+            case 9:
+                return 30;
+            case 10:
+                return 31;
+            case 11:
+                return 30;
+            case 12:
+                return 31;
+            default:
+                console.log("Mês incorreto");
+                break;
+        }
 }
+let numero = mesAno(2,2000);
+console.log(numero); */
+
+//quest 12
+/* function saberHora(começo,fim) {
+    let start = começo.split(':');
+    //console.log(start);
+    let end = fim.split(':');
+    //console.log(end);
+    if (start[0]>23|| end[0] >23 ||start[1]>59|| end[1] >59) {
+        return "Datas incorretas";
+    }
+    start = start[0]*60 + Number(start[1]);
+    end = end[0]*60 + Number(end[1]);
+   // console.log(end);
+    if (start<end) {
+        return start-end;
+    }else{
+        end += 1440;
+        return  end - start
+    }
+}
+let hora1 = saberHora("23:00", "04:10")
+let hora2 = saberHora("20:00", "23:69")
+console.log(hora1);
+console.log(hora2); */
+
+//quest 13
+
+/* function fibonacci(n) {
+    if (n === 0 || n === 1) {
+        return n;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+} */
