785cc8cb6f183eff9db1fbb4fb0719be2ec93962
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-09-15 12:27:07 +0000

    coisas

--- projeto/Cadastramento.js
+++ projeto/Cadastramento.js
@@ -36,88 +36,28 @@ while (loop) {
   let opcao = readline.questionInt("Escolha uma opcao: ");
   switch (opcao) {
     case 1:
-      let count = 1;
       console.log("Listando todos os pacientes");
       console.log("------------------------");
-      for (const p of listaPacientes) {
-        console.log(`Paciente ${count++}`);
-        console.log(p);
-      }
+      listar(listaPacientes);
       console.log("------------------------");
       readline.keyInPause();
       break;
     case 2:
-      let nome = readline.question("Digite o nome do paciente:");
-      let pergunta = readline.question("Tem nome social ?(s/n) ");
-      let nomeS;
-      if (pergunta == "S" || pergunta == "s") {
-        nomeS = readline.question("Digite seu nome: ");
-      }
-      let nomeMae = readline.question("Digite o nome da mãe:");
-      let cpf = readline.questionInt("Digite o cpf:");
-      let tel = readline.question("Digite o telefone:");
-      let dataNasc = readline.question("Digite a data de nascimento:");
-      let etnia = readline.question("Digite a etnia:");
-      let sexo = readline.question("Digite o sexo:");
-      let cep = readline.question("Digite o cep:");
-      let email = readline.question("Digite o email:");
-      let paciente;
-      if (pergunta == "S" || pergunta == "s") {
-        paciente = new Pessoa(nome, cpf, nomeMae, tel, dataNasc, etnia, sexo, cep, email, nomeS);
-      } else {
-        paciente = new Pessoa(nome, cpf, nomeMae, tel, dataNasc, etnia, sexo, cep, email);
-      }
-      listaPacientes.push(paciente);
+      cadastrar(listaPacientes);
       console.log("Paciente cadastrado com sucesso!");
       readline.keyInPause();
       break;
     case 3:
-      let cpfBusca = readline.questionInt(
-        "Cpf do paciente que deseja buscar: "
-      );
-      for (const a of listaPacientes) {
-        if (a.cpf === cpfBusca) {
-          console.log("Resultado da busca");
-          console.log("------------------------");
-          console.log(a);
-        }
-      }
+      buscar(listaPacientes);
       readline.keyInPause();
       break;
     case 4:
-      let cpfBusca1 = readline.questionInt(
-        "Cpf do paciente que deseja alterar: "
-      );
-      for (const a of listaPacientes) {
-        if (a.cpf === cpfBusca1) {
-          a.nome = readline.question("Digite o nome do paciente:");
-          a.pergunta = readline.question("Tem nome social ?(s/n) ");
-          if (pergunta == "S" || pergunta == "s") {
-            a.nomeS = readline.question("Digite seu nome: ");
-          }
-          a.nomeMae = readline.question("Digite o nome da mãe:");
-          a.cpf = readline.questionInt("Digite o cpf:");
-          a.tel = readline.question("Digite o telefone:");
-          a.dataNasc = readline.question("Digite a data de nascimento:");
-          a.etnia = readline.question("Digite a etnia:");
-          a.sexo = readline.question("Digite o sexo:");
-          a.cep = readline.question("Digite o cep:");
-          a.email = readline.question("Digite o email:");
-        }
-      }
+      alterar(listaPacientes);
       console.log("paciente alterado com sucesso!");
       readline.keyInPause();
       break;
     case 5:
-      let buscaCpf2 = readline.questionInt(
-        "Cpf do paciente que deseja alterar: "
-      );
-      for (const a of listaPacientes) {
-        if (a.cpf === buscaCpf2) {
-          console.log(`Excluido paciente ${a.nome} do sistema..."`);
-          listaPacientes.splice(a, 1);
-        }
-      }
+      remover(listaPacientes);
       console.log("paciente excluído com sucesso!");
       readline.keyInPause();
       break;
@@ -131,3 +71,168 @@ while (loop) {
   }
 }
 
+function listar(listaPacientes) {
+  let count = 0;
+  for (const p of listaPacientes) {
+    console.log(`Paciente ${count++}`);
+    console.log(p);
+  }
+}
+function cadastrar(listaPacientes) {
+  let nome = readline.question("Digite o nome do paciente:");
+  let pergunta = readline.question("Tem nome social ?(s/n) ");
+  let nomeS;
+  if (pergunta == "S" || pergunta == "s") {
+    nomeS = readline.question("Digite seu nome social: ");
+  }
+  let nomeMae = readline.question("Digite o nome da mãe:");
+  let cpf = readline.questionInt("Digite o cpf:");
+  let tel = readline.question("Digite o telefone:");
+  let dataNasc = readline.question("Digite a data de nascimento:");
+  let opcao = readline.questionInt("Digite a etnia\n1-Amarela\n2-Branca\n3-Preta\n4-Parda\n5-Indigena\n");
+  let etnia;
+  switch (opcao) {
+    case 1:
+      etnia = ETNIA.AMARELA;
+      break;
+
+    case 2:
+      etnia = ETNIA.BRANCA;
+      break;
+
+    case 3:
+      etnia = ETNIA.PRETA;
+      break;
+
+    case 4:
+      etnia = ETNIA.PARDA;
+      break;
+
+    case 5:
+      etnia = ETNIA.INDIGENA;
+      break;
+
+    default:
+      console.log("Valor errado");
+      etnia = "Undefined";
+      break;
+  }
+  opcao = readline.questionInt("Digite o sexo\n1-Masculino\n2-Femenino\n3-Outros\n");
+  let sexo;
+  switch (opcao) {
+    case 1:
+      sexo = SEXO.MASCULINO;
+      break;
+
+    case 2:
+      sexo = SEXO.FEMININO;
+      break;
+
+    case 3:
+      sexo = SEXO.OUTROS;
+      break;
+
+    default:
+      console.log("Valor errado");
+      sexo = "Undefined";
+      break;
+  }
+  let cep = readline.question("Digite o cep:");
+  let email = readline.question("Digite o email:");
+  let paciente;
+  if (pergunta == "S" || pergunta == "s") {
+    paciente = new Pessoa(nome, cpf, nomeMae, tel, dataNasc, etnia, sexo, cep, email, nomeS);
+  } else {
+    paciente = new Pessoa(nome, cpf, nomeMae, tel, dataNasc, etnia, sexo, cep, email);
+  }
+  listaPacientes.push(paciente);
+}
+function buscar(listaPacientes) {
+  let cpfBusca = readline.questionInt(
+    "Cpf do paciente que deseja buscar: "
+  );
+  for (const a of listaPacientes) {
+    if (a.cpf === cpfBusca) {
+      console.log("Resultado da busca");
+      console.log("------------------------");
+      console.log(a);
+    }
+  }
+}
+function alterar(listaPacientes) {
+  let cpfBusca = readline.questionInt(
+    "Cpf do paciente que deseja alterar: "
+  );
+  for (const a of listaPacientes) {
+    if (a.cpf === cpfBusca) {
+      a.setNome(readline.question("Digite o nome do paciente:"));
+      pergunta = readline.question("Tem nome social ?(s/n) ");
+      if (pergunta == "S" || pergunta == "s") {
+        a.setNomeS(readline.question("Digite seu nome social: "));
+      }
+      a.setNomeMae(readline.question("Digite o nome da mãe:"));
+      a.setCpf(readline.questionInt("Digite o cpf:"));
+      a.setTel(readline.question("Digite o telefone:"));
+      a.setDataNasc(readline.question("Digite a data de nascimento:"));
+      let opcao = readline.questionInt("Digite a etnia\n1-Amarela\n2-Branca\n3-Preta\n4-Parda\n5-Indigena\n");
+      switch (opcao) {
+        case 1:
+          a.setEtnia(ETNIA.AMARELA);
+          break;
+
+        case 2:
+          a.setEtnia(ETNIA.BRANCA);
+          break;
+
+        case 3:
+          a.setEtnia(ETNIA.PRETA);
+          break;
+
+        case 4:
+          a.setEtnia(ETNIA.PARDA);
+          break;
+
+        case 5:
+          a.setEtnia(ETNIA.INDIGENA);
+          break;
+
+        default:
+          console.log("Valor errado");
+          a.setEtnia("Undefined");
+          break;
+      }
+      opcao = readline.questionInt("Digite o sexo\n1-Masculino\n2-Femenino\n3-Outros\n");
+      switch (opcao) {
+        case 1:
+          a.setSexo(SEXO.MASCULINO)
+          break;
+
+        case 2:
+          a.setSexo(SEXO.FEMININO)
+          break;
+
+        case 3:
+          a.setSexo(SEXO.OUTROS)
+          break;
+
+        default:
+          console.log("Valor errado");
+          a.setSexo("Undefined");
+          break;
+      }
+      a.setCep(readline.question("Digite o cep:"));
+      a.setEmail(readline.question("Digite o email:"));
+    }
+  }
+}
+function remover(listaPacientes) {
+  let cpfBusca = readline.questionInt(
+    "Cpf do paciente que deseja excluir: "
+  );
+  for (const a of listaPacientes) {
+    if (a.cpf === cpfBusca) {
+      console.log(`Excluido paciente ${a.nome} do sistema..."`);
+      listaPacientes.splice(a, 1);
+    }
+  }
+}
--- projeto/Pessoa.js
+++ projeto/Pessoa.js
@@ -12,4 +12,66 @@ export default class Pessoa {
         this.cep = cep;
         this.email = email;
     }
+    
+    get getNome(){
+        return this.nome;
+    }
+    get getNomeS(){
+        return this.nomeS;
+    }
+    get getNomeMae(){
+        return this.nomeMae;
+    }
+    get getCpf(){
+        return this.cpf;
+    }
+    get getTel(){
+        return this.tel;
+    }
+    get getDataNasc(){
+        return this.dataNasc;
+    }
+    get getEtnia(){
+        return this.etnia;
+    }
+    get getSexo(){
+        return this.sexo;
+    }
+    get getCep(){
+        return this.cep;
+    }
+    get getEmail(){
+        return this.email;
+    }
+
+    set setNome(nome){
+        this.nome = nome;
+    }
+    set setNomeS(nomeS){
+        this.nomeS = nomeS;
+    }
+    set setNomeMae(nomeMae){
+        this.nomeMae = nomeMae;
+    }
+    set setCpf(cpf){
+        this.cpf = cpf;
+    }
+    set setTel(tel){
+        this.tel = tel;
+    }
+    set setDataNasc(dataNasc){
+        this.dataNasc = dataNasc;
+    }
+    set setEtnia(etnia){
+        this.etnia = etnia;
+    }
+    set setSexo(sexo){
+        this.sexo = sexo;
+    }
+    set setCep(cep){
+        this.cep = cep;
+    }
+    set setEmail(email){
+        this.email = email;
+    }
 }
