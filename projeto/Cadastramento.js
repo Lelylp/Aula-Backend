8ef76d190098c2f774d0975d05293d7f52708e61
Weslley Pereira <113145637+Lelylp@users.noreply.github.com>
2023-09-15 12:56:30 +0000

    add try-catch

--- projeto/Cadastramento.js
+++ projeto/Cadastramento.js
@@ -16,6 +16,7 @@ const SEXO = {
 
 let paciente0 = new Pessoa('Weslley', 84368224521, 'Mae', '81 9 85493221', '19/01/2001', ETNIA.BRANCA, SEXO.MASCULINO, '5024755', 'weslley3443@bol.com');
 let paciente1 = new Pessoa('Vagner', 54353454376, 'Outra mae', '88 9 38459845', '15/12/2002', ETNIA.PARDA, SEXO.FEMININO, '5589655', 'algo@hotmail.com', "Rogerio do creo");
+
 let listaPacientes = [];
 listaPacientes.push(paciente0);
 listaPacientes.push(paciente1);
@@ -79,18 +80,89 @@ function listar(listaPacientes) {
   }
 }
 function cadastrar(listaPacientes) {
-  let nome = readline.question("Digite o nome do paciente:");
+  let nome, nomeS, nomeMae, cpf, tel, dataNasc, etnia, sexo, cep, email
+  do {
+    try {
+      nome = readline.question("Digite o nome do paciente:");
+      if (!isNaN(nome)) {
+        throw 'Números não são permitidos'
+      }
+      if (nome.length < 3) {
+        throw 'Nome muito pequeno'
+      }
+    } catch (e) {
+      console.log(e + "\n");
+    }
+  } while (!isNaN(nome) || nome.length < 3);
+
   let pergunta = readline.question("Tem nome social ?(s/n) ");
-  let nomeS;
+  nomeS;
   if (pergunta == "S" || pergunta == "s") {
     nomeS = readline.question("Digite seu nome social: ");
   }
-  let nomeMae = readline.question("Digite o nome da mãe:");
-  let cpf = readline.questionInt("Digite o cpf:");
-  let tel = readline.question("Digite o telefone:");
-  let dataNasc = readline.question("Digite a data de nascimento:");
-  let opcao = readline.questionInt("Digite a etnia\n1-Amarela\n2-Branca\n3-Preta\n4-Parda\n5-Indigena\n");
-  let etnia;
+
+  do {
+    try {
+      nomeMae = readline.question("Digite o nome da mãe:");
+      if (!isNaN(nomeMae)) {
+        throw 'Números não são permitidos'
+      }
+      if (nomeMae.length < 3) {
+        throw 'Nome muito pequeno'
+      }
+    } catch (e) {
+      console.log(e + "\n");
+    }
+  } while (!isNaN(nomeMae) || nomeMae.length < 3);
+
+  do {
+    try {
+      cpf = readline.questionInt("Digite o cpf:");
+      if (isNaN(cpf)) {
+        throw 'Letras não são permitidos'
+      }
+      if (cpf.length != 11) {
+        throw 'Tamanho incorreto'
+      }
+    } catch (e) {
+      console.log(e + "\n");
+    }
+  } while (isNaN(cpf) || cpf.length != 11);
+
+  do {
+    try {
+      let ddd = readline.question("Digite o ddd do telefone:");
+      let numero = readline.question("Digite o número do telefone:");
+      tel = ddd + " " + numero
+      if (isNaN(tel)) {
+        throw 'Letras não são permitidos'
+      }
+      if (tel.length != 12) {
+        throw 'Número incorreto'
+      }
+    } catch (e) {
+      console.log(e + "\n");
+    }
+  } while (isNaN(tel) || tel.length != 12);
+
+  do {
+    try {
+      let dia = readline.question("Digite o dia que nasceu:");
+      let mes = readline.question("Digite o mes que nasceu:");
+      let ano = readline.question("Digite o ano que nasceu:");
+      dataNasc = dia + '/' +mes + '/' +ano;
+      if (isNaN(dia) || isNaN(mes) || isNaN(ano)) {
+        throw 'Letras não são permitidos'
+      }
+      if (dataNasc.length != 10) {
+        throw 'Tamanho incorreto'
+      }
+    } catch (e) {
+      console.log(e + "\n");
+    }
+  } while (isNaN(dia) || isNaN(mes) || isNaN(ano) || tel.length != 10);
+  opcao = readline.questionInt("Digite a etnia\n1-Amarela\n2-Branca\n3-Preta\n4-Parda\n5-Indigena\n");
+  etnia;
   switch (opcao) {
     case 1:
       etnia = ETNIA.AMARELA;
@@ -118,7 +190,7 @@ function cadastrar(listaPacientes) {
       break;
   }
   opcao = readline.questionInt("Digite o sexo\n1-Masculino\n2-Femenino\n3-Outros\n");
-  let sexo;
+  sexo;
   switch (opcao) {
     case 1:
       sexo = SEXO.MASCULINO;
@@ -137,8 +209,18 @@ function cadastrar(listaPacientes) {
       sexo = "Undefined";
       break;
   }
-  let cep = readline.question("Digite o cep:");
-  let email = readline.question("Digite o email:");
+
+  do {
+    try {
+      cep = readline.questionInt("Digite o cep:");
+      if (cep.length != 8) {
+        throw 'Tamanho incorreto'
+      }
+    } catch (e) {
+      console.log(e + "\n");
+    }
+  } while (cep.length != 8);
+  email = readline.question("Digite o email:");
   let paciente;
   if (pergunta == "S" || pergunta == "s") {
     paciente = new Pessoa(nome, cpf, nomeMae, tel, dataNasc, etnia, sexo, cep, email, nomeS);
@@ -233,6 +315,8 @@ function remover(listaPacientes) {
     if (a.cpf === cpfBusca) {
       console.log(`Excluido paciente ${a.nome} do sistema..."`);
       listaPacientes.splice(a, 1);
+    } else {
+      console.log("Esse paciente nao existe");
     }
   }
 }
